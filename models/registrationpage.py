5f807fbea01082f13f9d1c1df0d69f282d81c5cb
1OtaMan1 <oleksandr.makar.ol@gmail.com>
2018-03-28 20:10:51 +0300

    Added helper(generator). Updated registrationpage POM according to generator (need more work)

--- models/registrationpage.py
+++ models/registrationpage.py
@@ -1,49 +1,56 @@
 from selenium import webdriver
 from models.driver import MyDriver
+from models.Generators.emailgenerator import (generate_random_email, get_random_name,
+                                              get_random_digit, get_random_password)
 
 
 class RegistrationPage(MyDriver):
+    password = None
 
     def go_to_registration(self):
-        self.driver.driver.find_element_by_xpath('//*[@id="top-links"]/ul/li[2]/a').click()
-        self.driver.driver.find_element_by_xpath('//*[@id="top-links"]/ul/li[2]/ul/li[1]/a').click()
+        self.driver.find_element_by_xpath('//*[@id="top-links"]/ul/li[2]/a').click()
+        self.driver.find_element_by_xpath('//*[@id="top-links"]/ul/li[2]/ul/li[1]/a').click()
 
-    def input_firstname(self, firstname):
-        self.driver.driver.find_element_by_id("input-firstname").send_keys(firstname)
+    def input_firstname(self):
+        self.driver.find_element_by_id("input-firstname").send_keys(get_random_name(7))
 
-    def input_lastname(self, lastname):
-        self.driver.driver.find_element_by_id("input-lastname").send_keys(lastname)
+    def input_lastname(self):
+        self.driver.find_element_by_id("input-lastname").send_keys(get_random_name(7))
 
-    def input_email(self, email):
-        self.driver.driver.find_element_by_id("input-email").send_keys(email)
+    def input_email(self):
+        self.driver.find_element_by_id("input-email").send_keys(generate_random_email(5))
 
-    def input_telephone(self, telephone):
-        self.driver.driver.find_element_by_id("input-telephone").send_keys(telephone)
+    def input_telephone(self):
+        self.driver.find_element_by_id("input-telephone").send_keys(get_random_digit(9))
 
-    def input_password(self, password):
-        self.driver.driver.find_element_by_id("input-password").send_keys(password)
+    def input_password(self):
+        self.password = get_random_password(8)
+        self.driver.find_element_by_id("input-password").send_keys(self.password)
 
-    def input_confirm_password(self, confirm_password):
-        self.driver.driver.find_element_by_id("input-confirm").send_keys(confirm_password)
+    def input_confirm_password(self):
+        self.driver.find_element_by_id("input-confirm").send_keys(self.password)
 
     def check_checkbox(self):
-        self.driver.driver.find_element_by_name("agree").click()
+        self.driver.implicitly_wait(15)
+        self.driver.find_element_by_xpath('//*[@id="content"]/form/div/div/input[1]').click()
 
     def registration(self):
-        self.driver.driver.find_element_by_xpath('//*[@id="content"]/form/div/div/input[2]').click()
-
-# driver = webdriver.Chrome(executable_path=r'../webdrivers/chromedriver.exe')
-# base_url = "https://taqc296opencart.000webhostapp.com"
-# driver.get(base_url)
+        self.driver.find_element_by_xpath('//*[@id="content"]/form/div/div/input[2]').click()
 
 
-# page = RegistrationPage(driver)
-# page.go_to_registration()
-# page.input_firstname()
-# page.input_lastname()
-# page.input_email()
-# page.input_telephone()
-# page.input_password()
-# page.input_confirm_password()
-# page.check_checkbox()
-# page.registration()
+driver = webdriver.Chrome(executable_path=r'../webdrivers/chromedriver.exe')
+base_url = "https://taqc296opencart.000webhostapp.com"
+driver.get(base_url)
+
+
+page = RegistrationPage(driver)
+page.go_to_registration()
+page.input_firstname()
+page.input_lastname()
+page.input_email()
+page.input_telephone()
+page.input_password()
+page.input_confirm_password()
+page.check_checkbox()
+page.registration()
+
