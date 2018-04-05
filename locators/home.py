2674310a1fab1953e1e1ae8d990941d4de668245
schcurnazar <schcurnazar@gmail.com>
2018-04-05 23:07:52 +0300

    reformat home page add XPATH add home page metods

--- locators/home.py
+++ locators/home.py
@@ -1,7 +1,6 @@
 """
 TODO
 """
-from selenium.webdriver.common.by import By
 from .base import BasePageLocators
 
 
@@ -10,6 +9,3 @@ class HomePageLocators(BasePageLocators):
     """
     TODO
     """
-    COMPONENTS_TAB = (By.XPATH, '//*[@id="menu"]/div[2]/ul/li[3]/a')
-    MONITORS = (By.XPATH, '//*[@id="menu"]/div[2]/ul/li[3]/div/div/ul/li[2]/a')
-    PHONES = (By.XPATH, '//*[@id="menu"]/div[2]/ul/li[6]/a')
--- pages/home.py
+++ pages/home.py
@@ -1,17 +1,15 @@
 """
 Home Page comes here.
 """
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
 
 from locators.base import BasePageLocators
-from locators.home import HomePageLocators
 from .cart import CartPage
 from .login import LoginPage
 from .products import ProductsPage
 from .base import BasePage
 
 
+# pylint: disable=too-many-public-methods
 class HomePage(BasePage):
     """
     Home Page methods come here.
@@ -44,35 +42,60 @@ class HomePage(BasePage):
         """
         Click Component Tab.
         """
-        self.driver.find_element(*HomePageLocators.COMPONENTS_TAB).click()
+        self.driver.find_element(*BasePageLocators.COMPONENTS).click()
         return self
-
-    def click_nav_components_monitors(self):
-        """
-        Click Component Tab.
-        Click Monitors.
-        """
-        self.click_nav_components()
-        monitors = self.driver.find_element(*HomePageLocators.MONITORS)
-        wait = WebDriverWait(self.driver, 10)
-        wait.until(expected_conditions.element_to_be_clickable(HomePageLocators.MONITORS))
-        monitors.click()
-        return ProductsPage(self.driver)
 
     def click_nav_phones(self):
         """
         Click Phones Tab.
         """
-        phones = self.driver.find_element(*HomePageLocators.PHONES)
-        phones.click()
+        self.driver.find_element(*BasePageLocators.PHONES).click()
+
         return ProductsPage(self.driver)
 
     def click_nav_desktops(self):
         """
         Click Desktops Tab.
         """
-        self.driver.find_element(*HomePageLocators.DESKTOPS).click()
+        self.driver.find_element(*BasePageLocators.DESKTOPS).click()
         return self
+
+    def click_nav_laptops(self):
+        """
+        Click Laptops Tab.
+        """
+        self.driver.find_element(*BasePageLocators.LAPTOPS).click()
+        return self
+
+    def click_nav_tablets(self):
+        """
+        Click Tablests Tab.
+        """
+        self.driver.find_element(*BasePageLocators.TABLETS).click()
+        return ProductsPage(self.driver)
+
+    def click_nav_cameras(self):
+        """
+        Click Cameras Tab.
+        """
+        self.driver.find_element(*BasePageLocators.CAMERAS).click()
+        return ProductsPage(self.driver)
+
+    def click_nav_mp3_players(self):
+        """
+        Click MP3 Players Tab.
+        """
+        self.driver.find_element(*BasePageLocators.MP3S).click()
+        return self
+
+    def click_nav_desktops_pc(self):
+        """
+        Click Desktops Tab.
+        Click Pc.
+        """
+        self.click_nav_desktops()
+        self.driver.find_element(*BasePageLocators.DESKTOPS).click()
+        return ProductsPage(self.driver)
 
     def click_nav_desktops_mac(self):
         """
@@ -82,3 +105,117 @@ class HomePage(BasePage):
         self.click_nav_desktops()
         self.driver.find_element(*BasePageLocators.MAC).click()
         return ProductsPage(self.driver)
+
+    def click_nav_desktops_show_all(self):
+        """
+        Click Desktops Tab.
+        Click Show all desktops.
+        """
+        self.click_nav_desktops()
+        self.driver.find_element(*BasePageLocators.ALLDESKTOPS).click()
+        return ProductsPage(self.driver)
+
+    def click_nav_laptops_macs(self):
+        """
+        Click Laptops Tab.
+        Click Macs.
+        """
+        self.click_nav_laptops()
+        self.driver.find_element(*BasePageLocators.MACS).click()
+        return ProductsPage(self.driver)
+
+    def click_nav_laptops_windows(self):
+        """
+        Click Laptops Tab.
+        Click Windows.
+        """
+        self.click_nav_laptops()
+        self.driver.find_element(*BasePageLocators.WINDOWS).click()
+        return ProductsPage(self.driver)
+
+    def click_nav_laptops_show_all(self):
+        """
+        Click Laptops Tab.
+        Click Show all Laptops .
+        """
+        self.click_nav_laptops()
+        self.driver.find_element(*BasePageLocators.ALLLEPTOPS).click()
+        return ProductsPage(self.driver)
+
+    def click_nav_components_mice(self):
+        """
+        Click Components Tab.
+        Click Mice .
+        """
+        self.click_nav_components()
+        self.driver.find_element(*BasePageLocators.MICE).click()
+        return ProductsPage(self.driver)
+
+    def click_nav_components_monitors(self):
+        """
+        Click Components Tab.
+        Click Monitors .
+        """
+        self.click_nav_components()
+        self.driver.find_element(*BasePageLocators.MONITORS).click()
+        return ProductsPage(self.driver)
+
+    def click_nav_components_printers(self):
+        """
+        Click Components Tab.
+        Click Printers .
+        """
+        self.click_nav_components()
+        self.driver.find_element(*BasePageLocators.PRINTERS).click()
+        return ProductsPage(self.driver)
+
+    def click_nav_components_scanners(self):
+        """
+        Click Components Tab.
+        Click Scanners.
+        """
+        self.click_nav_components()
+        self.driver.find_element(*BasePageLocators.SCANNERS).click()
+        return ProductsPage(self.driver)
+
+    def click_nav_components_webcamera(self):
+        """
+        Click Components Tab.
+        Click Web Camera.
+        """
+        self.click_nav_components()
+        self.driver.find_element(*BasePageLocators.WEBCAMERAS).click()
+        return ProductsPage(self.driver)
+
+    def click_nav_components_show_all(self):
+        """
+        Click Components Tab.
+        Click Show all components.
+        """
+        self.click_nav_components()
+        self.driver.find_element(*BasePageLocators.ALLCOMPONENTS).click()
+        return ProductsPage(self.driver)
+
+    def get_components_list(self):
+        """
+        Find all subcategory Components
+        """
+
+        components_list = self.driver.find_elements(*BasePageLocators.LIST_COMPONENS)
+        return components_list
+
+    def get_desktops_list(self):
+        """
+        Find all subcategory Desktops
+        """
+
+        desktops_list = self.driver.find_elements(*BasePageLocators.LIST_DESKTOPS)
+        return desktops_list
+
+    def get_laptops_list(self):
+        """
+        Find all subcategory Desktops
+        """
+
+        laptops_list = self.driver.find_elements(*BasePageLocators.LIST_LAPTOPS)
+        return laptops_list
