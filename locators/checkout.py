3a53d29ee5fffea33e0374c191d591f90ca613a3
Kseniia Malinovskaia <kseniia.malinovskaia@gmail.com>
2018-04-05 15:31:03 +0300

    added checkout locatorls, methods and a row test for guest's purchase

--- locators/checkout.py
+++ locators/checkout.py
@@ -1,5 +1,5 @@
 """
-TODO
+Checkout Page locators come here
 """
 from selenium.webdriver.common.by import By
 from .base import BasePageLocators
@@ -8,11 +8,27 @@ from .base import BasePageLocators
 # pylint: disable=too-few-public-methods
 class CheckoutPageLocators(BasePageLocators):
     """
-    TODO
+    It contents constants
     """
-    REGISTER_ACCOUNT = (By.XPATH,
-                        '//*[@id="collapse-checkout-option"]/div/div/div[1]/div[1]/label/input')
-    GUEST_ACCOUNT = (By.XPATH,
-                     '//*[@id="collapse-checkout-option"]/div/div/div[1]/div[2]/label/input')
-    STEP_ONE_CONTINUE = (By.ID, 'button-account')
-    STEP_TWO_CONTINUE = (By.ID, 'button-register')
+    ACCORDION = (By.ID, 'accrodion')
+
+    GUEST_ACCOUNT = (By.CSS_SELECTOR, '[type="radio"][name="account"][value="guest"]')
+    REGISTER_ACCOUNT = (By.CSS_SELECTOR, '[type="radio"][name="account"][value="register"]')
+
+    BTN_ACCOUNT = (By.ID, 'button-account')
+    BTN_LOGIN = (By.ID, 'button-login')
+    BTN_GUEST = (By.ID, 'button-guest')
+
+    FIRST_NAME = (By.ID, 'input-payment-firstname')
+    LAST_NAME = (By.ID, 'input-payment-lastname')
+    EMAIL = (By.ID, 'input-payment-email')
+    TELEPHONE = (By.ID, 'input-payment-telephone')
+    FAX = (By.ID, 'input-payment-fax')
+
+    COMPANY = (By.ID, 'input-payment-company')
+    ADDRESS_1 = (By.ID, 'input-payment-address-1')
+    ADDRESS_2 = (By.ID, 'input-payment-address-2')
+    CITY = (By.ID, 'input-payment-city')
+    POST_CODE = (By.ID, 'input-payment-postcode')
+    COUNTRY = (By.ID, 'input-payment-country')
+    REGION_OR_STATE = (By.ID, 'input-payment-zone')
