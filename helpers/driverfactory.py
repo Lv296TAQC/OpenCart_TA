1f35911df305933807f30b2f81358ac75bca6b2f
SteTaras <undulatis@gmail.com>
2018-04-07 23:10:10 +0300

    Added simplest possible webdriver factory and conftest

--- helpers/driverfactory.py
+++ helpers/driverfactory.py
@@ -1,37 +1,33 @@
-""""Class to create singleton specified WebDriver"""
+""""
+Module contains class, which creates single DriverFactory that provides us specified WebDriver
+"""
 
 from selenium import webdriver
 
+
 # pylint: disable=too-few-public-methods
 class DriverFactory:
+    """"
+    Class to create single DriverFactory that provides us needed specified WebDriver
     """
-    TODO
-    """
-    _single_factory = None
-    _single_web_driver = None
 
-    def __new__(cls):
-        if cls._single_factory is None:
-            cls._single_factory = object.__new__(cls)
-        return cls._single_factory
-
-    @classmethod
-    def create_web_driver(cls, driver_name='chrome'):
+    @staticmethod
+    def get_driver(driver_name='chrome'):
         """
-        TODO
+        Create driver according to given driver text name
         """
-        if cls._single_web_driver is None:
-            if driver_name.lower() == 'chrome':
-                cls._single_web_driver = webdriver.Chrome()
-            elif driver_name.lower() == 'firefox':
-                cls._single_web_driver = webdriver.Firefox()
-            elif driver_name.lower() in ('ie', 'iexplorer', 'internetexplorer'):
-                cls._single_web_driver = webdriver.Ie()
-            elif driver_name.lower() == 'edge':
-                cls._single_web_driver = webdriver.Edge()
-            elif driver_name.lower() == 'safari':
-                cls._single_web_driver = webdriver.Safari()
-            else:
-                raise ValueError('Unknown type of browser')
-
-        return cls._single_web_driver
+        if driver_name.lower() == 'chrome':
+            _single_web_driver = webdriver.Chrome()
+        elif driver_name.lower() == 'firefox':
+            _single_web_driver = webdriver.Firefox()
+        elif driver_name.lower() in ('ie', 'iexplorer', 'internetexplorer'):
+            _single_web_driver = webdriver.Ie()
+        elif driver_name.lower() == 'edge':
+            _single_web_driver = webdriver.Edge()
+        elif driver_name.lower() == 'safari':
+            _single_web_driver = webdriver.Safari()
+        elif driver_name.lower() == 'opera':
+            _single_web_driver = webdriver.Opera()
+        else:
+            raise ValueError('Unknown name of browser')
+        return _single_web_driver
