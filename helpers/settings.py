ff777ee4e79fd0a1fa8aacf8885ca81a6163ca20
Kseniia Malinovskaia <kseniia.malinovskaia@gmail.com>
2018-04-11 13:15:03 +0300

    added basic config for logging

--- helpers/settings.py
+++ helpers/settings.py
@@ -1,7 +1,15 @@
 """
 Module contains setings.
 """
+import logging
 
 BASE_HOST = "http://localhost/opencart"
 BASE_USER_EMAIL = "taqc296@gmail.com"
 BASE_USER_PASSWORD = "root"
+
+logging.basicConfig(filename="sample.log",
+                    filemode='w',
+                    format=('# %(levelname)-8s [%(asctime)s] '
+                            '%(filename)-20s [LINE:%(lineno)s]   %(message)s'),
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    level=logging.INFO)
