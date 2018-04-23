9dee58b05e65146f3481b81a5c98d0820c602e92
sibirtsv <sibirtsv@gmail.com>
2018-04-23 12:00:39 +0300

    Added database presets

--- helpers/settings.py
+++ helpers/settings.py
@@ -3,6 +3,8 @@ Module contains settings.
 """
 import logging
 
+import os
+
 BASE_HOST = "http://localhost/opencart"
 PHONES = {
     "IPHONE": "iPhone",
@@ -13,6 +15,12 @@ BASE_USER_EMAIL = "taqc296@gmail.com"
 BASE_USER_PASSWORD = "root"
 BASE_CONNECTION = "mysql://root@localhost/opencart"
 URL = "http://127.0.0.1/index.php?route=product/product&product_id=47"
+
+PRESET_BASE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "dbpresets")
+DB_PRESET = {
+    "user_base": os.path.join(PRESET_BASE_PATH, "user", "base.sql"),
+    "guest_base": os.path.join(PRESET_BASE_PATH, "guest", "base.sql")
+}
 
 logging.basicConfig(filename="sample.log",
                     filemode='w',
